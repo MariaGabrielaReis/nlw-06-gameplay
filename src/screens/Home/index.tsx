3d4ae31e48e460359c241788f06ec4cb00bf2fd7
Maria Gabriela Reis <mariagabrielagreis@gmail.com>
2021-06-22 18:01:49 -0300

    Criação e estilização de componentes para finalização da home
    Cõmponentes para ícone de cada item da lista de partidas agendadas, bem como para cada item e o componente da própria lista, adicionando todos na tela home

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,14 +1,59 @@
 import React, { useState } from "react";
-import { View } from "react-native";
+import { View, Text, FlatList } from "react-native";
 
 import { styles } from "./styles";
 
 import { Profile } from "../../components/Profile";
 import { ButtonAdd } from "../../components/ButtonAdd";
 import { CategorySelect } from "../../components/CategorySelect";
+import { ListHeader } from "../../components/ListHeader";
+import { Appointment } from "../../components/Appointment";
+import { ListDivider } from "../../components/ListDivider";
 
 export function Home() {
   const [category, setCategory] = useState("");
+
+  const appointments = [
+    {
+      id: "1",
+      guild: {
+        id: "1",
+        name: "Lendários",
+        icon: null,
+        owner: true,
+      },
+      category: "2",
+      date: "21/07 às 21:00",
+      description:
+        "É hoje que vamos chegar ao challenger sem perder uma partida da md10",
+    },
+    {
+      id: "2",
+      guild: {
+        id: "1",
+        name: "Valorosos",
+        icon: null,
+        owner: true,
+      },
+      category: "4",
+      date: "21/07 às 21:00",
+      description:
+        "É hoje que vamos chegar ao challenger sem perder uma partida da md10",
+    },
+    {
+      id: "3",
+      guild: {
+        id: "1",
+        name: "Cozinheiros",
+        icon: null,
+        owner: true,
+      },
+      category: "3",
+      date: "21/07 às 21:00",
+      description:
+        "É hoje que vamos chegar ao challenger sem perder uma partida da md10",
+    },
+  ];
 
   function handleCategorySelect(categoryId: string) {
     categoryId === category ? setCategory("") : setCategory(categoryId);
@@ -21,10 +66,21 @@ export function Home() {
         <ButtonAdd />
       </View>
 
-      <View>
-        <CategorySelect
-          categorySelected={category}
-          setCategory={handleCategorySelect}
+      <CategorySelect
+        categorySelected={category}
+        setCategory={handleCategorySelect}
+      />
+
+      <View style={styles.content}>
+        <ListHeader title={"Partidas agendadas"} subtitle={"Total: 6"} />
+
+        <FlatList
+          data={appointments}
+          keyExtractor={(item) => item.id}
+          renderItem={({ item }) => <Appointment data={item} />}
+          ItemSeparatorComponent={() => <ListDivider />}
+          style={styles.matches}
+          showsVerticalScrollIndicator={false}
         />
       </View>
     </View>
