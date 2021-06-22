c04fd255c803da9fa98f16246a13eac823dc3d09
Maria Gabriela Reis <mariagabrielagreis@gmail.com>
2021-06-22 15:44:23 -0300

    Criação e estilização de componente de botão para adicionar algo

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -4,12 +4,14 @@ import { View } from "react-native";
 import { styles } from "./styles";
 
 import { Profile } from "../../components/Profile";
+import { ButtonAdd } from "../../components/ButtonAdd";
 
 export function Home() {
   return (
     <View style={styles.container}>
       <View style={styles.header}>
         <Profile />
+        <ButtonAdd />
       </View>
     </View>
   );
