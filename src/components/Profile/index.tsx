956e63faa6932b618a15e5ef77790256c7d935ad
Maria Gabriela Reis <mariagabrielagreis@gmail.com>
2021-06-25 19:18:43 -0300

    Uso dos dados do usuário na home pelo contexto

--- src/components/Profile/index.tsx
+++ src/components/Profile/index.tsx
@@ -4,15 +4,18 @@ import { View, Text } from "react-native";
 import { styles } from "./styles";
 
 import { Avatar } from "../Avatar";
+import { useAuth } from "../../hooks/auth";
 
 export function Profile() {
+  const { user } = useAuth();
+
   return (
     <View style={styles.container}>
-      <Avatar urlImage={"https://github.com/MariaGabrielaReis.png"} />
+      <Avatar urlImage={user.avatar} />
       <View>
         <View style={styles.user}>
           <Text style={styles.greeting}>Olá,</Text>
-          <Text style={styles.username}>Maby</Text>
+          <Text style={styles.username}>{user.firstName}</Text>
         </View>
 
         <Text style={styles.message}>Hoje é dia de vitória </Text>
