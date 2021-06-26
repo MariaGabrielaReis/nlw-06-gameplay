5ef7e492de67ef6d81e6481289e92c0864b4ddbc
Maria Gabriela Reis <mariagabrielagreis@gmail.com>
2021-06-26 08:55:54 -0300

    Aplicando persistência de dados da autenticação com local storage

--- src/hooks/auth.tsx
+++ src/hooks/auth.tsx
@@ -1,7 +1,14 @@
 /* centralização da parte do contexto */
-import React, { createContext, useContext, useState, ReactNode } from "react";
+import React, {
+  createContext,
+  useContext,
+  ReactNode,
+  useState,
+  useEffect,
+} from "react";
 
 import * as AuthSession from "expo-auth-session";
+import AsyncStorage from "@react-native-async-storage/async-storage";
 
 const { REDIRECT_URI } = process.env;
 const { SCOPE } = process.env;
@@ -10,6 +17,7 @@ const { CLIENT_ID } = process.env;
 const { CDN_IMAGE } = process.env;
 
 import { api } from "../services/api";
+import { COLLECTION_USERS } from "../configs/database";
 
 type User = {
   id: string;
@@ -67,11 +75,15 @@ function AuthProvider({ children }: AuthProviderProps) {
         const firstName = userInfo.data.username.split(" ")[0];
         userInfo.data.avatar = `${CDN_IMAGE}/avatars/${userInfo.data.id}/${userInfo.data.avatar}.png`;
 
-        setUser({
+        const userData = {
           ...userInfo.data,
           firstName,
           token: params.access_token,
-        });
+        };
+
+        await AsyncStorage.setItem(COLLECTION_USERS, JSON.stringify(userData));
+
+        setUser(userData);
       }
     } catch {
       throw new Error("Não foi possível autenticar");
@@ -80,6 +92,21 @@ function AuthProvider({ children }: AuthProviderProps) {
       setLoading(false);
     }
   }
+
+  async function loadUserStorageData() {
+    const storage = await AsyncStorage.getItem(COLLECTION_USERS);
+
+    if (storage) {
+      const userLogged = JSON.parse(storage) as User;
+      api.defaults.headers.authorization = `Bearer ${userLogged.token}`;
+
+      setUser(userLogged);
+    }
+  }
+
+  useEffect(() => {
+    loadUserStorageData();
+  }, []);
 
   return (
     <AuthContext.Provider value={{ user, loading, signIn }}>
